3e06e4dd4ab64c5a862536f4afbdda0aa88c4fe4
Jefferson Reis <jefaokpta@hotmail.com>
2020-01-22 10:46:16 -0300

    inicio

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <ul>
+    <li v-for="fruta of frutas" :class="{highlighted: fruta.status}" v-on:click="fruta.status=!fruta.status" >
+      {{fruta.tipo}}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      frutas: [
+        {tipo: 'banana',status: false},
+        {tipo: 'maca',status: false},
+        {tipo: 'framboesa',status: false}
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+.highlighted{
+  background-color: orange;
+}
+</style>
